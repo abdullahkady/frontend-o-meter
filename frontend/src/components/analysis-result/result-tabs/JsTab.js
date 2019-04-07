1d9596e6e5023ecfca6221bab8f1d43ec3c67609
Abdullah Kady <abdullahkady@gmail.com>
2019-04-07 10:06:15 +0200

    Add sorting to JS result

--- frontend/src/components/analysis-result/result-tabs/JsTab.js
+++ frontend/src/components/analysis-result/result-tabs/JsTab.js
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
+import getNestedProperty from "lodash/get";
+import { FILE_CHOICES } from "../../../constants/js-metrics";
 import JsResultCard from "../result-cards/JsResultCard";
 import chunkArray from "../../../utils/chunkArray";
+import { Row, Col, Dropdown } from "react-bootstrap";
 
-export default props => {
-  const { data: files } = props;
-  const rows = chunkArray(files, 3);
+class JsTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortBy: ""
+    };
+  }
+  onSortChanged = sortType => {
+    this.setState({ sortBy: sortType });
+  };
 
-  return rows.map((chunk, i) => (
-    <React.Fragment>
-      <Row key={i}>
-        {chunk.map((file, ix) => (
-          <Col md={4} key={ix}>
-            <JsResultCard ix={ix} data={file} />
-          </Col>
+  render() {
+    let { data: files } = this.props;
+    files = files.sort(
+      (f1, f2) =>
+        getNestedProperty(f2.metrics, this.state.sortBy) -
+        getNestedProperty(f1.metrics, this.state.sortBy)
+    );
+    const rows = chunkArray(files, 3);
+    return (
+      <React.Fragment>
+        <Dropdown onSelect={this.onSortChanged}>
+          <Dropdown.Toggle variant="primary" id="dropdown-basic">
+            SORT BY
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {Object.keys(FILE_CHOICES).map((optionKey, i) => (
+              <Dropdown.Item
+                key={i}
+                eventKey={FILE_CHOICES[optionKey]}
+                as="button"
+              >
+                {optionKey}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+
+        {rows.map((chunk, i) => (
+          <React.Fragment key={i}>
+            <Row>
+              {chunk.map((file, ix) => (
+                <Col md={4} key={ix}>
+                  <JsResultCard data={file} />
+                </Col>
+              ))}
+            </Row>
+            <br />
+          </React.Fragment>
         ))}
-      </Row>
-      <br />
-    </React.Fragment>
-  ));
-};
+      </React.Fragment>
+    );
+  }
+}
+
+export default JsTab;
---
+++ frontend/src/constants/js-metrics.js
@@ -0,0 +1,16 @@
+/*
+  Maps the display name (key) to the property path in the response object
+*/
+
+export const FILE_CHOICES = {
+  "Total Cyclomatic Complexity": "aggregate.cyclomatic",
+  "Total Cyclomatic Density": "aggregate.cyclomaticDensity",
+  "Total Halstead Bugs": "aggregate.halstead.bugs",
+  "Total Halstead Difficulty": "aggregate.halstead.difficulty",
+  "Total Halstead Effort": "aggregate.halstead.effort",
+  "Total Halstead Length": "aggregate.halstead.length",
+  "Total Halstead Time": "aggregate.halstead.time",
+  "Average Function Argument Count": "methodAverage.paramCount",
+  "Total Lines of Code(logical)": "aggregate.sloc.logical",
+  "Maintainability Index": "maintainability"
+};
