1772be74bdf6753455183aaf7ce2dcf1199cdf1f
Abdullah Kady <abdullahkady@gmail.com>
2019-04-08 12:46:52 +0200

    Add sort type (asc/desc)

--- frontend/src/components/analysis-result/result-tabs/JsTab.js
+++ frontend/src/components/analysis-result/result-tabs/JsTab.js
@@ -3,13 +3,23 @@ import getNestedProperty from "lodash/get";
 import { FILE_SORT_CHOICES } from "../../../constants/js-metrics";
 import JsResultCard from "../result-cards/JsResultCard";
 import chunkArray from "../../../utils/chunkArray";
-import { Row, Col, Dropdown } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Dropdown,
+  Navbar,
+  Form,
+  InputGroup,
+  Button,
+  Container
+} from "react-bootstrap";
 
 class JsTab extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sortOption: ""
+      sortOption: "",
+      isAscending: false
     };
   }
 
@@ -23,42 +33,64 @@ class JsTab extends Component {
 
   render() {
     let { data: files } = this.props;
-    const { sortOption } = this.state;
+    const { sortOption, isAscending } = this.state;
     const sortOptionObjectPath = FILE_SORT_CHOICES[sortOption];
     files = files.sort(
       (f1, f2) =>
-        getNestedProperty(f2.metrics, sortOptionObjectPath) -
-        getNestedProperty(f1.metrics, sortOptionObjectPath)
+        getNestedProperty(
+          (isAscending ? f1 : f2).metrics,
+          sortOptionObjectPath
+        ) -
+        getNestedProperty((isAscending ? f2 : f1).metrics, sortOptionObjectPath)
     );
     const rows = chunkArray(files, 3);
     return (
       <React.Fragment>
-        <Dropdown onSelect={this.onSortChanged}>
-          <Dropdown.Toggle variant="primary" id="dropdown-basic">
-            {this.state.sortOption}
-          </Dropdown.Toggle>
+        <Navbar className="bg-light justify-content-between">
+          <Form inline>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>SORT</InputGroup.Text>
+              </InputGroup.Prepend>
+              <Dropdown onSelect={this.onSortChanged}>
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                  {this.state.sortOption}
+                </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            {Object.keys(FILE_SORT_CHOICES).map((optionKey, i) => (
-              <Dropdown.Item key={i} eventKey={optionKey} as="button">
-                {optionKey}
-              </Dropdown.Item>
-            ))}
-          </Dropdown.Menu>
-        </Dropdown>
-
-        {rows.map((chunk, i) => (
-          <React.Fragment key={i}>
-            <Row>
-              {chunk.map((file, ix) => (
-                <Col md={4} key={ix}>
-                  <JsResultCard data={file} />
-                </Col>
-              ))}
-            </Row>
-            <br />
-          </React.Fragment>
-        ))}
+                <Dropdown.Menu>
+                  {Object.keys(FILE_SORT_CHOICES).map((optionKey, i) => (
+                    <Dropdown.Item key={i} eventKey={optionKey} as="div">
+                      {optionKey}
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+              <InputGroup.Append>
+                <Button
+                  className="shadow-none"
+                  variant="outline-secondary"
+                  onClick={() => this.setState({ isAscending: !isAscending })}
+                >
+                  {isAscending ? "ASC" : "DESC"}
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+        </Navbar>
+        <Container style={{ marginTop: "15px" }}>
+          {rows.map((chunk, i) => (
+            <React.Fragment key={i}>
+              <Row>
+                {chunk.map((file, ix) => (
+                  <Col md={4} key={ix}>
+                    <JsResultCard data={file} />
+                  </Col>
+                ))}
+              </Row>
+              <br />
+            </React.Fragment>
+          ))}
+        </Container>
       </React.Fragment>
     );
   }
