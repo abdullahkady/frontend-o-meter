5717cca500f5fed4634f2a4bba52d75036da0086
Abdullah Kady <abdullahkady@gmail.com>
2019-05-06 09:06:27 +0200

    Update container name

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppContainer from './containers/App';
+import AppContainer from './containers/AppContainer';
 
 ReactDOM.render(<AppContainer />, document.getElementById('root'));
